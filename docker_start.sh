bda6955a342727596444cf636a037c57821eee79
tony24681379 <yellow24681379@gmail.com>
2016-03-31 20:25:24 +0800

    fix docker_start.sh mount data folder

--- docker_start.sh
+++ docker_start.sh
@@ -7,4 +7,4 @@ fi
 if [ "$(docker ps -aqf "name=oj_web")" != "" ]; then
 	docker rm -f oj_web
 fi
-docker run -d -p 80:8000 -v $(pwd)/data:/var/nthuoj -v $(pwd)/media:/NTHUOJ_web/media --name=oj_web --cpu-shares 20 --restart=always oj_web
+docker run -d -p 80:8000 -v $(pwd)/data:/var/nthuoj -v $(pwd)/NTHUOJ_web:/NTHUOJ_web -v $(pwd)/media:/NTHUOJ_web/media --name=oj_web --cpu-shares 20 --restart=always oj_web
